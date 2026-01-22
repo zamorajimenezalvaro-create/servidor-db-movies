2cf8d3905b7bd7771af64cfce58a1d15530725a0
zamorajimenezalvaro-create <zamorajimenezalvaro@gmail.com>
2026-01-22 11:29:45 +0100

    filtros de busqueda

--- db/models/movies.ts
+++ db/models/movies.ts
@@ -1,5 +1,15 @@
 import { Database } from "bun:sqlite"
 
+export function getMovies(db : Database, filters: {title: string | undefined, genres: string | undefined}) {
+const {title, genres} = filters;
+let movies 
+if(title && genres) movies = getAllMoviesByTitleAndGenre(db, title, genres)
+    else if (title) movies = getAllMoviesByTitle(db, title)
+    else if (genres) movies = getAllMoviesByGenre(db, genres)
+    else movies = getAllMovies(db)
+return movies
+}
+
 export function getAllMovies(db: Database) {
     const query = db.query(`SELECT * FROM movies`)
     return query.all()
@@ -11,6 +21,16 @@ export function getAllMoviesById(db: Database, id: number) {
 }
 
 export function getAllMoviesByTitle(db: Database, title: string ) {
-    const query = db.query(`SELECT * FROM movies WHERE title LIKE ? LIMIT 1`)
+    const query = db.query(`SELECT * FROM movies WHERE title LIKE ?`)
     return query.all(`%${title}%`)
+}
+
+export function getAllMoviesByGenre(db: Database, genres: string ) {
+    const query = db.query(`SELECT * FROM movies WHERE title LIKE ? AND genres LIKE ?`)
+    return query.all(`%${genres}%`)
+}
+
+export function getAllMoviesByTitleAndGenre(db: Database, title: string, genres: string ) {
+    const query = db.query(`SELECT * FROM movies WHERE title LIKE ? AND genres LIKE ?`)
+    return query.all(`%${title}%`, `%${genres}%`)
 }
--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
 import { initDB } from "./db/init"
-import { getAllMovies, getAllMoviesById, getAllMoviesByTitle } from "./db/models/movies"
+import { getMovies } from "./db/models/movies"
 import express from "express"
+import type { Request, Response, NextFunction } from "express"
 const app = express();
 const PORT = 3000
 const db = await initDB()
 
+const logMiddleware = (req: Request,
+    res: Response, next: NextFunction) => {
+    console.log(req.method, req.url, new Date().toDateString())
+    next()
+    if (req.method === "GET") {
+        console.log(req.query)
+    }
+}
+
+app.use(logMiddleware)
+
 app.get("/movies", (req, res) => {
-    const movies = getAllMovies(db)
-    res.json(movies)
+const { title, genre } = req.query
+const filters = {
+    title: typeof title === "string" ? title: undefined,
+    genre: typeof genre === "string" ? genre: undefined
+}
+const movies = getMovies(db, filters)
+res.json(movies)
 })
 
 app.listen(PORT, () => {
